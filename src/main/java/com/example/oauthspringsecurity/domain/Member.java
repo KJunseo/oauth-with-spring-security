eebd3a95859000c7bb721f5a319048590eb578d7
kimjunseo <air.junseo@gmail.com>
2021-07-23 00:15:18 +0900

    feat: 사전 작업(도메인 생성 & 의존성 추가)

---
+++ src/main/java/com/example/oauthspringsecurity/domain/Member.java
@@ -0,0 +1,42 @@
+package com.example.oauthspringsecurity.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String imageUrl;
+
+    protected Member() {
+    }
+
+    public Member(Long id, String name, String email, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}

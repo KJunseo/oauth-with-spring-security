82c20475a0130a5608c87b909170fb8b863f32cb
kimjunseo <air.junseo@gmail.com>
2021-07-23 02:23:23 +0900

    feat: 도메인 정보 추가

---
+++ src/main/java/com/example/oauthspringsecurity/domain/Role.java
@@ -0,0 +1,16 @@
+package com.example.oauthspringsecurity.domain;
+
+public enum Role {
+    GUEST("ROLE_GUEST"),
+    USER("ROLE_USER");
+
+    private final String key;
+
+    Role(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}

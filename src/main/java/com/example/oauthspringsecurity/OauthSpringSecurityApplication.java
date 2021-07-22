dd020da2e96a0d5d05d2960f77016ba09235b442
kimjunseo <air.junseo@gmail.com>
2021-07-23 00:13:42 +0900

    Initial commit

---
+++ src/main/java/com/example/oauthspringsecurity/OauthSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauthspringsecurity/OauthSpringSecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.oauthspringsecurity;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

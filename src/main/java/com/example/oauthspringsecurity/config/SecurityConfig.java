f5398a277f6f5c4e4fcc5636067eddd751ad4250
kimjunseo <air.junseo@gmail.com>
2021-07-27 13:54:35 +0900

    docs: 설명 추가

--- src/main/java/com/example/oauthspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/oauthspringsecurity/config/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .headers().frameOptions().disable()
+        http.csrf().disable() // h2 console 접속을 위해
+            .headers().frameOptions().disable() // h2 console 접속을 위해
             .and()
             .oauth2Login() // OAuth2 로그인 설정 시작점
             .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
